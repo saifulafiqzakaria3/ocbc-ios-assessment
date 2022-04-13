2755f572aa40b24795e8293c2f275ac98cefadb3
Saiful Afiq <saiful.afiq@hexainitiative.com>
2022-04-13 14:37:50 +0800

    [] LoginView: add login spinner when waiting for API to respond

--- Module/Screen/Login/LoginViewController.swift
+++ Module/Screen/Login/LoginViewController.swift
@@ -17,6 +17,12 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var registerButton: UIButton!
+    let loginSpinner: UIActivityIndicatorView = { () -> UIActivityIndicatorView in
+        let loginSpinner = UIActivityIndicatorView(style: .medium)
+        loginSpinner.translatesAutoresizingMaskIntoConstraints = false
+        loginSpinner.hidesWhenStopped = true
+        return loginSpinner
+    }()
     
     var viewModel: LoginViewModel!
     let disposeBag = DisposeBag()
@@ -25,7 +31,7 @@ class LoginViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         setupDisplay()
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel = LoginViewModel()
@@ -37,18 +43,53 @@ class LoginViewController: UIViewController {
     func setupDisplay() {
         usernameErrorLabel.isHidden = true
         passwordErrorLabel.isHidden = true
+        usernameErrorLabel.text = "Username is required"
+        passwordErrorLabel.text = "Password is required"
+        
+        loginButton.addSubview(loginSpinner)
+        loginSpinner.centerXAnchor.constraint(equalTo: loginButton.centerXAnchor).isActive = true
+        loginSpinner.centerYAnchor.constraint(equalTo: loginButton.centerYAnchor).isActive = true
+        
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
     }
     
     func transformInput() {
         viewModel.loginButtonTapped = loginButton.rx.tap.asDriver()
         viewModel.registerButtonTapped = registerButton.rx.tap.asDriver()
+
+        let usernameValidate = usernameTextField.rx.text.map({$0?.isEmpty})
+        let passwordValidate = passwordTextField.rx.text.map({$0?.isEmpty})
+        let enableLoginButton = Observable.combineLatest(usernameValidate, passwordValidate, viewModel.isLoading).map({ (nameIsEmpty, passwordIsEmpty, isLoading) in
+            return !(nameIsEmpty ?? true) && !(passwordIsEmpty ?? true) && !isLoading
+        })
         
         disposeBag.insert(
             usernameTextField.rx.text <-> viewModel.username,
-            passwordTextField.rx.text <-> viewModel.password
+            passwordTextField.rx.text <-> viewModel.password,
+            enableLoginButton.bind(to: loginButton.rx.isEnabled),
+            viewModel.isLoading.bind(to: loginSpinner.rx.isAnimating)
         )
     }
+    
+}
 
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.text == nil || textField.text!.count < 1 {
+            if textField === usernameTextField {
+                usernameErrorLabel.isHidden = false
+            } else {
+                passwordErrorLabel.isHidden = false
+            }
+        } else {
+            if textField === usernameTextField {
+                usernameErrorLabel.isHidden = true
+            } else {
+                passwordErrorLabel.isHidden = true
+            }
+        }
+    }
 }
 
 
