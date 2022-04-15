68ff902164ff112bca80624a91f5b115321fcb75
Saiful Afiq <saiful.afiq@hexainitiative.com>
2022-04-15 23:12:28 +0800

    [] URLRequest: Refactor urlrequest extension for post with parameter function

--- Extensions/URLRequest+Extensions.swift
+++ Extensions/URLRequest+Extensions.swift
@@ -26,25 +26,8 @@ extension URLRequest {
             return try JSONDecoder().decode(T.self, from: data)
         }
     }
-    
+
     static func postWithParamaters<T>(resource: Resource<T>) -> Observable<T> {
-        guard let param = resource.parameter else { return Observable.empty() }
-        
-        return Observable.just(resource.url).flatMap { url -> Observable<Data> in
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-            request.httpBody = try? JSONSerialization.data(withJSONObject: param, options: .prettyPrinted)
-            //status code is 200..<300 and only then return data, if not then it will throw an error
-            return URLSession.shared.rx.data(request: request)
-        }.map { data -> T in
-            return try JSONDecoder().decode(T.self, from: data)
-        }
-    }
-    
-    
-    static func post<T>(resource: Resource<T>) -> Observable<T> {
         guard let param = resource.parameter else { return Observable.empty() }
         
         return Observable.just(resource.url).flatMap { url -> Observable<(response: HTTPURLResponse, data: Data)> in
@@ -53,14 +36,35 @@ extension URLRequest {
             request.addValue("application/json", forHTTPHeaderField: "Content-Type")
             request.addValue("application/json", forHTTPHeaderField: "Accept")
             request.httpBody = try? JSONSerialization.data(withJSONObject: param, options: .prettyPrinted)
-            
+            //status code is 200..<300 and only then return data, if not then it will throw an error
             return URLSession.shared.rx.response(request: request)
         }.map { (response, data) -> T in
-            print("Response: ", response)
-            print("Data: ", data)
             return try JSONDecoder().decode(T.self, from: data)
         }
     }
+    
+    
+//    static func post<T>(resource: Resource<T>) -> Observable<T> {
+//        guard let param = resource.parameter else { return Observable.empty() }
+//
+//        return Observable.just(resource.url).flatMap { url -> Observable<(response: HTTPURLResponse, data: Data)> in
+//            var request = URLRequest(url: url)
+//            request.httpMethod = "POST"
+//            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+//            request.addValue("application/json", forHTTPHeaderField: "Accept")
+//            request.httpBody = try? JSONSerialization.data(withJSONObject: param, options: .prettyPrinted)
+//
+//            return URLSession.shared.rx.response(request: request)
+//        }.map { (response, data) -> T in
+//            print("Response: ", response)
+//            if response.statusCode >= 300 {
+//                if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: String] {
+//                    print("Failure Response: \(json["error"])")
+//                }
+//            }
+//            return try JSONDecoder().decode(T.self, from: data)
+//        }
+//    }
 }
 
 
