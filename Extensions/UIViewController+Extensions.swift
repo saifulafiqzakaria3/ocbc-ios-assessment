d20cfa5a7aafc9d264f69c08add57c295e1e0c3a
Saiful Afiq <saiful.afiq@hexainitiative.com>
2022-04-15 23:12:38 +0800

    Create UIViewController+Extensions.swift

---
+++ Extensions/UIViewController+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewController+Extensions.swift
+//  ocbc-ios-assessment
+//
+//  Created by Saiful.Afiq on 15/04/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
